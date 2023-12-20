07bd22487debd247adedb8d49153e21565d89e9e
Paul Rushing <paul@rushing.net>
2023-12-20 14:14:10 -0600

    comment out things that will incure cost

--- 04-variables-and-outputs/web-app/main.tf
+++ 04-variables-and-outputs/web-app/main.tf
@@ -187,21 +187,21 @@ resource "aws_lb" "load_balancer" {
 
 }
 
-resource "aws_route53_zone" "primary" {
-  name = var.domain
-}
+# resource "aws_route53_zone" "primary" {
+#   name = var.domain
+# }
 
-resource "aws_route53_record" "root" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = var.domain
-  type    = "A"
+# resource "aws_route53_record" "root" {
+#   zone_id = aws_route53_zone.primary.zone_id
+#   name    = var.domain
+#   type    = "A"
 
-  alias {
-    name                   = aws_lb.load_balancer.dns_name
-    zone_id                = aws_lb.load_balancer.zone_id
-    evaluate_target_health = true
-  }
-}
+#   alias {
+#     name                   = aws_lb.load_balancer.dns_name
+#     zone_id                = aws_lb.load_balancer.zone_id
+#     evaluate_target_health = true
+#   }
+# }
 
 # resource "aws_db_instance" "db_instance" {
 #   allocated_storage   = 20
